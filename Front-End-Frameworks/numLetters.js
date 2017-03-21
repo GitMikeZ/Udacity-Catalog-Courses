b50087b99f356aa4b28f5e38f1b21866afeef804
Mike Z <minqi03@gmail.com>
2017-03-20 21:16:53 -0700

    Update

---
+++ Front-End-Frameworks/numLetters.js
@@ -0,0 +1,9 @@
+var numLetters = function(letter) {
+    return new Function('times',
+        "if (times < 0) return ''; \
+        var result = ''; \
+        times = Math.round(times); \
+        while(times--) { result += '" + letter + "'; } \
+        return result;"
+    );
+}
